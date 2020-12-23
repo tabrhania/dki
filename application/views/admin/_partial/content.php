63ae9f499d490739ea251bcebb40b47edb8da2bf
tabrhania <44232533+tabrhania@users.noreply.github.com>
2020-12-23 10:24:19 +0700

    update

---
+++ application/views/admin/_partial/content.php
@@ -0,0 +1,70 @@
+<div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid">
+                        <h1 class="mt-4">Daftar SPJ</h1>
+                        
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <i class="fas fa-table mr-1"></i>
+                                Data SPJ
+                            </div>
+                            <div class="card-body">  
+                                <div class="table-responsive">
+                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Nama SPJ</th>
+                                                <th>Pembuat</th>
+                                                <th>Tanggal Pembuatan</th>
+                                                <th>Perubahan Terakhir</th>
+                                                <th>User Pengubah</th>
+                                                
+                                            </tr>
+                                        </thead>
+                                        
+                                        <tbody>
+                                            
+                                            <tr>
+                                                <td>1</td>
+                                                <td>Display Informasi</td>
+                                                <td>Fandi</td>
+                                                <td>27 Januari 2020</td>
+                                                <td>30 Januari 2020</td>
+                                                <td>Nuril</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Brielle Williamson</td>
+                                                <td>Integration Specialist</td>
+                                                <td>New York</td>
+                                                <td>61</td>
+                                                <td>2012/12/02</td>
+                                                <td>$372,000</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Herrod Chandler</td>
+                                                <td>Sales Assistant</td>
+                                                <td>San Francisco</td>
+                                                <td>59</td>
+                                                <td>2012/08/06</td>
+                                                <td>$137,500</td>
+                                            </tr>
+                                            
+                                           
+                                           
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </main>
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Your Website 2020</div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
---
+++ application/views/admin/_partial/sidenav.php
@@ -0,0 +1,32 @@
+<div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <!-- dibawah ini heading bisa untuk namae -->
+                            <div class="sb-sidenav-menu-heading"></div> 
+                            <a class="nav-link" href="index.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                SPJ
+                            </a>
+
+                            <a class="nav-link" href="index.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Hari Libur
+                            </a>
+
+                            <a class="nav-link" href="index.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Akun
+                            </a>
+
+                        
+                        
+                            
+                        </div>
+                    </div>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Logged in as:</div>
+                        Admin
+                    </div>
+                </nav>
+            </div>
